13f403a0f7dc3026d75c3778b2b3c8b0f605680d
Scott Weldon <scott@scott-weldon.com>
2015-12-31 19:30:22 -0800

    Use Composer command to set OAuth key

--- recipes/laravel.rb
+++ recipes/laravel.rb
@@ -44,19 +44,7 @@ end
 
 
 if !node['laravel']['github_oauth'].empty?
-  # FIXME set this dynamically.
-  user_home = "/root"
-
-  composer_path = "#{user_home}/.composer"
-
-  if !::File.directory?("#{composer_path}")
-    execute "mkdir --parents #{composer_path}"
-  end
-
-  template "#{composer_path}/auth.json" do
-    source "github-oauth.json.erb"
-    mode "0644"
-  end
+  execute "cd #{path}; #{composer_command} config github-oauth.github.com #{node['laravel']['github_oauth']}"
 end
 
 # Check if the composer config files already exist
